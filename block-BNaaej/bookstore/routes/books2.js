e7e647b1a5dfc4266a3425db2243a015f90d78df
Palacharla Narendra <palacharlanarendra@gmail.com>
2021-08-26 00:24:22 +0530

    bookstore categories added

--- block-BNaaej/bookstore/routes/books2.js
+++ block-BNaaej/bookstore/routes/books2.js
@@ -92,4 +92,107 @@ router.delete('/newcomments/:id', (req, res, next) => {
     );
   });
 });
+//create a category
+router.put('/:id/addCategory', (req, res, next) => {
+  let id = req.params.id;
+  req.body.category = req.body.category.trim().split(',');
+  Newbook.findByIdAndUpdate(id, req.body, (err, book) => {
+    if (err) return next(err);
+    res.status(200).json({ book });
+  });
+});
+
+//edit a category
+router.put('/:id/editCategory', (req, res, next) => {
+  let id = req.params.id;
+  req.body.category = req.body.category.trim().split(',');
+  Newbook.findByIdAndUpdate(id, req.body, (err, book) => {
+    if (err) return next(err);
+    res.status(200).json({ book });
+  });
+});
+
+//list all categories
+router.get('/list/Category', (req, res, next) => {
+  Newbook.find({})
+    .distinct('category')
+    .exec((err, category) => {
+      if (err) return next(err);
+      res.status(200).json({ category });
+    });
+});
+
+//list books by category
+router.get('/list/byCategory', (req, res, next) => {
+  Newbook.aggregate([{ $unwind: '$category' }]).exec((err, books) => {
+    if (err) return next(err);
+    res.status(200).json({ books });
+  });
+});
+
+//count books by category
+router.get('/countby/category', (req, res, next) => {
+  Newbook.aggregate([
+    { $group: { _id: '$category', count: { $sum: 1 } } },
+    { $unwind: '$category' },
+  ]).exec((err, result) => {
+    if (err) return next(err);
+    res.status(200).json({ result });
+  });
+});
+
+//list book by author
+router.get('/filter/byauthor', (req, res, next) => {
+  Newbook.aggregate([
+    {
+      $group: {
+        _id: '$author',
+        books: {
+          $push: '$$ROOT',
+        },
+      },
+    },
+  ]).exec((err, result) => {
+    if (err) return next(err);
+    res.status(200).json({ result });
+  });
+});
+
+//add tags to books
+router.put('/:id/addtags', (req, res, next) => {
+  let id = req.params.id;
+  req.body.tags = req.body.tags.trim().split(',');
+  Newbook.findByIdAndUpdate(id, req.body, (err, book) => {
+    if (err) return next(err);
+    res.status(200).json({ book });
+  });
+});
+
+//list all tags in ascending
+router.get('/filter/tags', (req, res, next) => {
+  Newbook.aggregate([{ $unwind: '$tags' }, { $sort: { tags: 1 } }]).exec(
+    (err, result) => {
+      if (err) return next(err);
+      res.status(200).json({ result });
+    }
+  );
+});
+
+//filter books by tags
+router.get('/list/tags', (req, res, next) => {
+  Newbook.aggregate([{ $unwind: '$tags' }]).exec((err, result) => {
+    if (err) return next(err);
+    res.status(200).json({ result });
+  });
+});
+//countby tags
+router.get('/countby/tags', (req, res, next) => {
+  Newbook.aggregate([
+    { $unwind: '$tags' },
+    { $group: { _id: '$tags', count: { $sum: 1 } } },
+  ]).exec((err, tags) => {
+    if (err) return next(err);
+    res.status(200).json({ tags });
+  });
+});
 module.exports = router;
